35a0abda7db69475c107bc086c546c12af521919
Francinyn <francinyn@hotmail.com>
2024-03-26 09:15:04 -0300

    testando

---
+++ Aula07/exemplo_sobrecarga.js
@@ -0,0 +1,13 @@
+class Operacoes {
+    constructor(){
+
+    }
+
+    Somar(valorA, valorB){
+        console.log("Resultado: " + (valorA + valorB));
+    }
+
+}
+
+var objeto = new Operacoes();
+objeto.Somar(10,20);
